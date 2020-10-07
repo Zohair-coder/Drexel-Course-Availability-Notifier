7b5819a2777df3009f27627a47d6f63c0b417174
Zohair-coder <zohair.ul.hasan@gmail.com>
2020-10-07 18:48:12 +0500

    Added basic check for course code

--- drexel_course_url_finder.py
+++ drexel_course_url_finder.py
@@ -16,10 +16,9 @@ def find():
     sp1 = printAndFindSeasonSP1()
     printColleges()
     chosen_college = input("Please select your college index: ")
-    find_course = input("Please enter the course code (EXMPL 101): ")
     # creates list with first element containing code string and second element containing code number
     print("Finding your course name...")
-    find_course = find_course.split()
+    find_course = inputCourse()
     courses_data = findCourse(chosen_college, find_course, sp1)
     print("Done.")
     print("Finding all sections of your course...")
@@ -111,6 +110,31 @@ def printAndFindSeasonSP1():
         if 'sp' in variable:
             sp1 = variable
     return sp1
+
+
+def inputCourse():
+    """
+    Function that prommpts the user to input the course code in the format: "EXMPL 101".
+    If the input is not valid(two words, first word composed of alphabet and second word composed of numbers), the user is asked to enter the code again.
+    If the first word is in lower-case, it is converted to uppercase.
+
+    Arguments: None
+
+    Returns: List of two elements, with the first element being EXMPL and second element being 101.
+    """
+
+    isValid = False
+    while not isValid:
+        course = input("Please enter the course code (EXMPL 101): ")
+        course = course.split()
+        if len(course) == 2 and course[0].isalpha() and course[1].isnumeric():
+            isValid = True
+        else:
+            print("Invalid input.")
+
+    if course[0].islower():
+        course[0] = course[0].upper()
+    return course
 
 
 if __name__ == "__main__":
