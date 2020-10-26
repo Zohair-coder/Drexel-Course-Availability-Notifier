4c09c694e9f66a17554a06cf9f901b72c5e54837
Zohair-coder <zohair.ul.hasan@gmail.com>
2020-10-26 18:51:14 +0500

    Added an optional program drexel_course_information.py. Not necessary for the main program, added just for convenience

---
+++ drexel_course_information.py
@@ -0,0 +1,43 @@
+"""
+Program that gives you information about a course. Not required for Drexel Course Availability Notifier.
+Added just for convenience.
+To start, execute: 
+"python drexel_course_information.py"
+in your terminal.
+"""
+
+
+import drexel_course_url_finder
+import utility
+from tabulate import tabulate
+
+
+def getTimeAndDays(soup):
+    table_data = soup.select("td")
+    for td in table_data:
+        if td.get_text()[-2:] == "pm" or td.get_text()[-2:] == "am":
+            return td.get_text() + " - " + td.findNext('td').get_text()
+    return None
+
+
+url = drexel_course_url_finder.find()
+soup = utility.getSoup(url)
+
+
+class_details = [["CRN ", utility.getData("CRN", soup)], [
+    "Section ", utility.getData("Section", soup)],
+    ["Credits ", utility.getData("Credits", soup)],
+    ["Title ", utility.getData("Title", soup)],
+    ["Campus ", utility.getData("Campus", soup)],
+    ["Instructor ", utility.getData(
+        "Instructor(s)", soup)],
+    ["Instruction Type ", utility.getData(
+        "Instruction Type", soup)],
+    ["Instruction Method ", utility.getData(
+        "Instruction Method", soup)],
+    ["Max Enroll ", utility.getData("Max Enroll", soup)],
+    ["Enroll ", utility.getData("Enroll", soup)],
+    ["Time and Days ", getTimeAndDays(soup)],
+]
+
+print(tabulate(class_details))
